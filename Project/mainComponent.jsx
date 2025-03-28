d69ca547abbacd5244983581e21da1de0b668d1f
mousavi <mousavics2005@gmail.com>
2025-03-28 15:59:06 +0330

    prop

--- Project/mainComponent.jsx
+++ Project/mainComponent.jsx
@@ -1,25 +1,26 @@
-export default function Main() {
+export default function Main(props) {
     return (
         <section>
-            <img className="main-img" src="./public/mount-fuji.jpg" alt="" />
+
+            <div className="img-container">
+                <img className="main-img" src={props.img.src} alt={props.img.alt} />
+            </div>
             
             <div className="text-container">
                 <div className="location">
                     <img className="location-icon"  src='./public/location.svg' alt="" />
                     <span className="country">Japan</span>
-                    <a href="https://www.google.com/maps/place/Mount+Fuji/@35.360625,138.7273634,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA!5m1!1e4?entry=ttu&g_ep=EgoyMDI1MDMyNC4wIKXMDSoASAFQAw%3D%3D">view on google maps</a>
+                    <a href= {props.googleMap}>view on google maps</a>
                 </div>
 
                 <div className="detail-text">
-                    <h2 className="location-name">Mount Fuji</h2>
-                    <span className="date">12 Jan, 2021 - 24 Jan, 2021</span>
-                    <p>Mount fuji is the tallest mountain in japan, standing at 3776 meters.
-                        Mount fuji is the single most popular tourist site in japan, for both japanese
-                        and foreign tourists.
-                        </p>
+                    <h2 className="location-name">{props.locationName}</h2>
+                    <span className="date">{props.time}</span>
+                    <p className="text"> {props.text} </p>
                 </div>
 
             </div>
+
         </section>
     )
 }
--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -9,8 +9,22 @@ function App() {
   return (
     <>
       <Head />
-      <Main />
-      <Main />
+      <Main 
+            img = {{src:"./public/mount-fuji.jpg", alt:"mount fuji image"}}
+            country = 'Japan'
+            googleMap = 'https://www.google.com/maps/place/Mount+Fuji/@35.360625,138.7273634,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA!5m1!1e4?entry=ttu&g_ep=EgoyMDI1MDMyNC4wIKXMDSoASAFQAw%3D%3D'
+            locationName = 'Mount Fuji'
+            time = '12 Jan, 2021 - 24 Jan, 2021'
+            text = 'Mount fuji is the tallest mountain in japan, standing at 3776 meters.Mount fuji is the single most popular tourist site in japan, for both japanese and foreign tourists.'
+      />
+      <Main 
+            img = {{src:"./public/mount-fuji.jpg", alt:"mount fuji image"}}
+            country = 'Japan'
+            googleMap = 'https://www.google.com/maps/place/Mount+Fuji/@35.360625,138.7273634,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA!5m1!1e4?entry=ttu&g_ep=EgoyMDI1MDMyNC4wIKXMDSoASAFQAw%3D%3D'
+            locationName = 'Mount Fuji'
+            time = '12 Jan, 2021 - 24 Jan, 2021'
+            text = 'Mount fuji is the tallest mountain in japan, standing at 3776 meters.Mount fuji is the single most popular tourist site in japan, for both japanese and foreign tourists.'    
+      />
     </>
   )
 }
