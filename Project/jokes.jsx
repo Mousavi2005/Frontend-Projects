a8d7ab510fd863042af614852a79f5cd084b4da0
mousavi <mousavics2005@gmail.com>
2025-03-29 15:54:28 +0330

    test

---
+++ Project/jokes.jsx
@@ -0,0 +1,14 @@
+export default [
+    {
+        setup: "I got my daughter a fridge for her birthday.",
+        punchline: "I can't wait to see her face light up when she opens it."
+    },
+    {
+        setup: "How did the hacker escape the police?",
+        punchline: "He just ransomware!"
+    },
+    {
+        setup: "",
+        punchline: "I failed math so many times at school, I can't even count."
+    },
+]
--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -2,15 +2,22 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Head from '../headComponent'
-import Main from '../mainComponent'
+import JokeComponent from '../jokeComponent'
+import jokes from '../jokes'
+
+const array = jokes.map(function(j) {
+  return (
+    <JokeComponent 
+      setup = {j.setup}
+      punchline = {j.punchline}
+    />
+  )
+})
 
 function App() {
   return (
     <>
-      <Head />
-      <Main />
-      <Main />
+      {array}
     </>
   )
 }
