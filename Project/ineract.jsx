013f2bebd0781e6a93f13e31551ca4e687ea9fe1
mousavi <mousavics2005@gmail.com>
2025-04-03 21:06:14 +0330

    form

--- Project/ineract.jsx
+++ Project/ineract.jsx
@@ -2,25 +2,23 @@ import React from "react"
 import IngredientComponent from "./ingredientComponent";
 
 export default function Interact() {
+
     let [element, setElement] = React.useState([])
 
-    function addIngredient(e) {
-        e.preventDefault();
+    function addIngredient(formData) {
 
-        const input = document.querySelector("input")
-        const value = input.value
+        const value = formData.get('ingredient')
         setElement(prev => [...prev,value])
 
-        input.value = ''
-
-      }
+    }
 
     return (
         <>
-            <form onSubmit={addIngredient} className="interact">
-                <input type="text" placeholder="e.g rice"/>
+            <form action={addIngredient} className="interact">
+                <input name="ingredient" type="text" placeholder="e.g rice"/>
                 <button  className="add-ingredient-btn">+ Add ingredient</button>
             </form>
+            
             <div className='body-head-text'>
                 <h2>Ingredients on hand : </h2>
             </div>
--- Project/ingredientComponent.jsx
+++ Project/ingredientComponent.jsx
@@ -1,14 +1,7 @@
 // import { element } from "./ineract"
 
 export default function IngredientComponent(prop) {
-    // console.log(prop.name)
     return (
-        // <>
-        
-        // <li>{element}</li>
         <li>{prop.name}</li>
-        // <p>{prop.name}</p>
-        
-        // </>
     )
 }
