40fe3a1f143f4284b506b0ff06c6666fccfe526c
mousavi <mousavics2005@gmail.com>
2025-04-23 19:58:12 +0330

    Update AiComponent.jsx

--- Project/jokeComponent.jsx
+++
@@ -1,8 +0,0 @@
-export default function JokeComponent(prop) {
-    return (
-        <div className="joke">
-            <p>{prop.setup}</p>
-            <p>{prop.punchline}</p>
-        </div>
-    )
-}
